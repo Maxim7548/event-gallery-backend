3ccc84f5c5b297e6b7cf60b1b74f7630d098dd14
Maxim7548 <maksimmartynov705@gmail.com>
2026-04-02 03:31:18 +0300

    Delete Message.js

--- Message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const messageSchema = new mongoose.Schema({
-    text: { type: String, required: true },
-    time: { type: String, required: true },
-    id: { type: String }
-}, { timestamps: true });
-
-export const Message = mongoose.model('Message', messageSchema);
