72da86dc00795621574d19597fffb538d8234065
Maxim7548 <maksimmartynov705@gmail.com>
2026-04-02 12:27:09 +0300

    Add files via upload

--- models/Event.js
+++ models/Event.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 const eventSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String },
-    date: { type: Date, default: Date.now },
-    creator: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  title: { type: String, required: true },
+  description: { type: String },
+  date: { type: String },
+  organizer: { type: String }, 
+  image: { type: String },     
+  creator: { type: String }
 });
 
 export const Event = mongoose.model('Event', eventSchema);
