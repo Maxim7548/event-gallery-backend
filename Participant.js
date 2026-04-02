8b37cd79e1289be7e363104de7312e6191f1fd9e
Maxim7548 <maksimmartynov705@gmail.com>
2026-04-02 03:30:50 +0300

    Delete Participant.js

--- Participant.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const participantSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    eventId: { type: String, required: true }
-}, { timestamps: true }); 
-
-export const Participant = mongoose.model('Participant', participantSchema);
